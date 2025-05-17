bd3571bc9c7ede6ff00e54202c5f5a348103cd69
Lintangeaa <lindprak@gmail.com>
2025-05-18 01:20:12 +0700

    update

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -8,11 +8,19 @@ const ecosystem = [
     max_memory_restart: '200M',
     env: {
       NODE_ENV: 'development',
+      PORT: 5000,
+      DATABASE_URL: 'mysql://user:password@localhost:3306/dbname',
+      VERIFY_TOKEN:
+        'EAAP9ticn5usBO4ZB5I3nUq4AExwH87D3X36gK9R6Mq14M7h5Qk8z4XjTZBKPJzpNKZCcPl6YJSwplLhUkp30CvlxDADqc84ZAJZCkqsvwLNkYefCbAWBFmjESd9EZB1zzhBj53x52Jbsvxjf0g3qIdxAEeqbZC7uCKt88JDeUC7ZCAYjSE2eQBZCalQL9LTQ2OGInjNDymcTUEGmKUsnGGZA5GgirOhaYZD',
     },
     env_production: {
       NODE_ENV: 'production',
+      PORT: 5000,
+      DATABASE_URL: 'mysql://user:password@localhost:3306/dbname',
+      VERIFY_TOKEN:
+        'EAAP9ticn5usBO4ZB5I3nUq4AExwH87D3X36gK9R6Mq14M7h5Qk8z4XjTZBKPJzpNKZCcPl6YJSwplLhUkp30CvlxDADqc84ZAJZCkqsvwLNkYefCbAWBFmjESd9EZB1zzhBj53x52Jbsvxjf0g3qIdxAEeqbZC7uCKt88JDeUC7ZCAYjSE2eQBZCalQL9LTQ2OGInjNDymcTUEGmKUsnGGZA5GgirOhaYZD',
     },
   },
 ];
 
-export default { apps: ecosystem };
+module.exports = { apps: ecosystem };
