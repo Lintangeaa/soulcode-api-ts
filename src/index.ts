acdabdbe668e7ae052bd2ea59050ea3f77c89804
Lintangeaa <lindprak@gmail.com>
2025-05-18 00:33:07 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (_req, res) => {
-  res.send('✅ Hello from Express with TypeScript and native import syntax!');
+  res.send('✅ Hello World!');
 });
 
 app.listen(port, () => {
