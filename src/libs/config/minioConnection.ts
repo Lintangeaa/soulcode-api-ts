f56a496f1b92910eaa75681e8bc0f75638020646
Lintangeaa <lindprak@gmail.com>
2025-06-05 00:00:19 +0700

    fix: comment minio

--- src/libs/config/minioConnection.ts
+++ src/libs/config/minioConnection.ts
@@ -1,12 +1,12 @@
-import { Client } from 'minio';
-import { minioConfig } from './index.js';
+// import { Client } from 'minio';
+// import { minioConfig } from './index.js';
 
-const minioConnection = new Client({
-  endPoint: minioConfig.ENDPOINT,
-  port: 9000,
-  useSSL: false,
-  accessKey: minioConfig.ACCESS_KEY,
-  secretKey: minioConfig.SECRET_KEY,
-});
+// const minioConnection = new Client({
+//   endPoint: minioConfig.ENDPOINT,
+//   port: 9000,
+//   useSSL: false,
+//   accessKey: minioConfig.ACCESS_KEY,
+//   secretKey: minioConfig.SECRET_KEY,
+// });
 
-export default minioConnection;
+// export default minioConnection;
